21aacd1965883bd02a38f82ac29ac3451049fd32
psytau <rmanninoFoo@gmail.com>
2014-09-27 20:15:57 +0800

    added more tests

--- tests/index.js
+++ tests/index.js
@@ -1,23 +1,63 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
+/*global $, describe, it, expect*/
+'use strict';
+
+describe('A suite', function() {
+  it('contains spec with an expectation', function() {
     expect(true).toBe(true);
   });
 });
 
-describe("Segmenting Simple English Sentences", function() {
-  it("Can segment a sentence in a <p>", function() {
-    $('<div id="test-simple-eng"></div>').appendTo('body')
-     .append('<p id="test-simple-eng-p">Ladybugs lounge in hamocks made for them.</p>');
+var addTextHelper = function (content) {
+  $('<div id="test-div"></div>').appendTo('body')
+   .append(content);
+};
+var removeTextHelper = function () {
+  $('#test-div').remove();
+};
 
-     var tokens = $('#test-simple-eng-p').findTokens();
-     console.log(tokens);
-     expect(tokens.length).toBe(13);
-     expect(tokens[0]).toBe('Ladybugs');
-     expect(tokens[1]).toBe(' ');
-     expect(tokens[2]).toBe('lounge');
-     expect(tokens[12]).toBe('them');
+describe('Segmenting Simple Sentences', function() {
 
+  it('Can segment an English sentence in a <p>', function() {
+    var text = '<p id="test-simple-eng-p">Ladybugs lounge in hamocks made for them.</p>';
+    addTextHelper(text);
 
-     $('#test-simple-eng').remove();
+    var tokens = $('#test-simple-eng-p').findTokens();
+    expect(tokens.length).toBe(13);
+    expect(tokens[0]).toBe('Ladybugs');
+    expect(tokens[1]).toBe(' ');
+    expect(tokens[2]).toBe('lounge');
+    expect(tokens[12]).toBe('them');
+
+    removeTextHelper();
+  });
+
+  it('Can segment a Chinese sentence in a <p>', function() {
+    var text = '<p id="test-simple-zh-p">毛毛蟲是一種蟲子啦。</p>';
+    addTextHelper(text);
+    var tokens = $('#test-simple-zh-p').findTokens();
+
+    expect(tokens.length).toBe(5);
+    expect(tokens[0]).toBe('毛毛蟲');
+    expect(tokens[1]).toBe('是');
+    expect(tokens[2]).toBe('一種');
+    expect(tokens[3]).toBe('蟲子');
+    expect(tokens[4]).toBe('啦');
+
+    removeTextHelper();
+  });
+
+  it('Can segment a Chinese sentence with several tags', function() {
+    var text = '<p id="test-complex-zh-p">毛毛蟲<b>是<u>一種</u>蟲子</b>啦。</p>';
+    addTextHelper(text);
+    var tokens = $('#test-complex-zh-p').findTokens();
+
+    expect(tokens.length).toBe(5);
+    expect(tokens[0]).toBe('毛毛蟲');
+    expect(tokens[1]).toBe('是');
+    expect(tokens[2]).toBe('一種');
+    expect(tokens[3]).toBe('蟲子');
+    expect(tokens[4]).toBe('啦');
+
+    removeTextHelper();
   });
 });
